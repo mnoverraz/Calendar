66d9c204f3361c836d8469f9090ef9370f00aa1d
Stefan Meier <stefan.r.meier@unil.ch>
2012-05-21 22:45:55 +0200

    Message body emtpy bug fixed

--- src/calendar/web/renderer/Message.java
+++ src/calendar/web/renderer/Message.java
@@ -5,5 +5,5 @@ import java.util.HashMap;
 
 public class Message {
 	public boolean success = true;
-	public ArrayList<HashMap<String, Object>> body;
+	public ArrayList<HashMap<String, Object>> body = new ArrayList<HashMap<String, Object>>();
 }
