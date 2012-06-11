686d54008230bba5d120df65247d960e8b8c76e4
Stefan Meier <stefan.r.meier@unil.ch>
2012-06-11 20:19:12 +0200

    Room insert fixed

--- appClientModule/Main.java
+++ appClientModule/Main.java
@@ -18,7 +18,7 @@ import calendar.core.session.RoomHandlerRemote;
 public class Main {
 	public static void main(String[] args) throws NamingException,
 			ParseException, PersistException {
-		//addEvents();
+		addEvents();
 		addRoomsAndCategories();
 	}
 	
