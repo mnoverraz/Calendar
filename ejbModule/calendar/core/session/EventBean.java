9ff9d1814d170fbe32b099cf078e4e4f19e854da
Stefan Meier <stefan.r.meier@unil.ch>
2012-06-14 17:01:36 +0200

    Entity fixed

--- ejbModule/calendar/core/session/EventBean.java
+++ ejbModule/calendar/core/session/EventBean.java
@@ -1,5 +1,6 @@
 package calendar.core.session;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -10,6 +11,7 @@ import javax.persistence.PersistenceException;
 import javax.persistence.Query;
 
 import calendar.core.entity.Event;
+import calendar.core.entity.EventDate;
 
 /**
  * Session Bean implementation class EventHandler
@@ -35,29 +37,35 @@ public class EventBean implements EventHandlerLocal, EventHandlerRemote {
 		if (params == null)
 			params = new HashMap<String, Object>();
 
-		query.append("SELECT event");
+
 		query.append(" FROM Event as event");
+		query.append(" WHERE");
+		query.append(" 1=1");
 		if (params.containsKey("id")) 
-			query.append(" WHERE event.id = :id");
+			query.append(" AND event.id = :id");
+		/*
+		if (params.containsKey("start"))
+			query.append(" AND date.start >= :start");
+		if (params.containsKey("end"))
+			query.append(" AND date.end <= :end");*/
+		
 		else {
-			if (params.containsKey("start") || params.containsKey("end"))
+			/*if (params.containsKey("start") || params.containsKey("end")) {
 				query.append(" LEFT OUTER JOIN event.eventDates as date");
-			if (params.containsKey("start"))
-				query.append(" WHERE date.start >= :start");
-			if (params.containsKey("end"))
-				query.append(" AND date.end <= :end");
+				query.append(" WHERE event.id = date.event.id");
+			}*/
+
 		}
 		
 		try {
 			Query q = em.createQuery(query.toString());
 			if (params.containsKey("id"))
 				q.setParameter("id", params.get("id"));
-			else {
-				if (params.containsKey("start"))
-					q.setParameter("start", params.get("start"));
-				if (params.containsKey("end"))
-					q.setParameter("end", params.get("end"));
-			}
+			/*if (params.containsKey("start"))
+				q.setParameter("start", params.get("start"));
+			if (params.containsKey("end"))
+				q.setParameter("end", params.get("end"));*/
+			
 			events = q.getResultList();
 		} catch (PersistenceException ex) {
 			ex.printStackTrace();
--- ejbModule/calendar/core/session/RoomBean.java
+++ ejbModule/calendar/core/session/RoomBean.java
@@ -31,11 +31,20 @@ public class RoomBean implements RoomHandlerRemote, RoomHandlerLocal {
 			throws PersistException {
 		List<Room> rooms = null;
 		StringBuffer query = new StringBuffer();
+		
+		if (params == null)
+			params = new HashMap<String, Object>();
 
 		query.append("FROM Room room ");
+		query.append(" WHERE");
+		query.append(" 1=1");
+		if (params.containsKey("id")) 
+			query.append(" AND room.id = :id");
 		
 		try {
 			Query q = em.createQuery(query.toString());
+			if (params.containsKey("id"))
+				q.setParameter("id", params.get("id"));
 			//q.setParameter("id", null);
 			
 			rooms = q.getResultList();
