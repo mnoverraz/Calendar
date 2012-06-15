35b4f1c185a4f6a30690928256cffb9a9fc06f6b
Stefan Meier <stefan.r.meier@unil.ch>
2012-06-15 13:46:28 +0200

    small bug fix

--- appClientModule/Main.java
+++ appClientModule/Main.java
@@ -13,9 +13,9 @@ import calendar.core.entity.RoomCategory;
 import calendar.core.session.EventHandlerRemote;
 import calendar.core.session.PersistException;
 import calendar.core.session.RoomCategoryHandlerRemote;
-import calendar.core.session.RoomHandlerRemote;
 
 public class Main {
+	private static Context context;
 	public static void main(String[] args) throws NamingException,
 			ParseException, PersistException {
 		addEvents();
@@ -23,15 +23,10 @@ public class Main {
 	}
 	
 	public static void addEvents() throws NamingException, ParseException, PersistException {
-		Context context;
 		EventHandlerRemote eventHandler;
 		context = new InitialContext();
 		eventHandler = (EventHandlerRemote) context
 				.lookup("calendarEAR/EventBean/remote");
-		/*
-		 * Dummy data for test, real data access should be here
-		 */
-
 
 		Event event = new Event(0, "event 1",
 				"description 1", "m");
@@ -70,41 +65,28 @@ public class Main {
 		event.addEventDate(eventDate);
 
 		eventHandler.create(event);
-
 	}
 	
 	public static void addRoomsAndCategories() throws NamingException, ParseException, PersistException {
-		
-		Context context;
-		RoomHandlerRemote roomHandler;
 		RoomCategoryHandlerRemote roomCategoryHandler;
 		context = new InitialContext();
-		roomHandler = (RoomHandlerRemote) context
-				.lookup("calendarEAR/RoomBean/remote");
 		roomCategoryHandler = (RoomCategoryHandlerRemote) context
 				.lookup("calendarEAR/RoomCategoryBean/remote");
-		/*
-		 * Dummy data for test, real data access should be here
-		 */
-//'219', 'Salle de conférence', '', '12 pl. assises + 9 chaises, rétro-projecteur, tableau blanc, vidéoconférence', 0, 1),
+
 		Room room1 = new Room(0, "313", "Microscope à fluorescence", "Please indicate if you use fluorescence or not (fluo oui, fluo non).");
 		Room room2 = new Room(0, "219", "Salle de conférence", "12 pl. assises + 9 chaises, rétro-projecteur, tableau blanc, vidéoconférence");
-		Room room3 = new Room(0, "006", "Animalerie", "");
-		
-//		roomHandler.create(room1);
-//		roomHandler.create(room2);
-//		roomHandler.create(room3);
+		Room room3 = new Room(0, "006", "Animalerie", "Only permitted persons can access the animal facility.");
 
-		RoomCategory roomCategory1 = new RoomCategory(0, "Room");
-		RoomCategory roomCategory2 = new RoomCategory(0, "Hall");
+		RoomCategory roomCategory1 = new RoomCategory(0, "Appareil scientifique");
+		RoomCategory roomCategory2 = new RoomCategory(0, "Salles");
+		RoomCategory roomCategory3 = new RoomCategory(0, "Animalerie");
 
 		roomCategory1.addRoom(room1);
-		roomCategory1.addRoom(room2);
-		roomCategory2.addRoom(room3);
+		roomCategory2.addRoom(room2);
+		roomCategory3.addRoom(room3);
 
 		roomCategoryHandler.create(roomCategory1);
-		roomCategoryHandler.create(roomCategory2);
-		
+		roomCategoryHandler.create(roomCategory2);	
+		roomCategoryHandler.create(roomCategory3);	
 	}
-	
 }
