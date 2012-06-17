c10c92a5ff4bb2c7fe2cb01124002f4e7ec6b3d8
Stefan Meier <stefan.r.meier@unil.ch>
2012-06-17 21:01:17 +0200

    trashing

--- ejbModule/calendar/core/exception/CoreException.java
+++
@@ -1,9 +0,0 @@
-package calendar.core.exception;
-
-public abstract class CoreException extends Exception {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	public Object detailInformation;
-}
